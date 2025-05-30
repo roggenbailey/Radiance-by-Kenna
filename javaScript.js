dee2d4b380e3793b6e4c1eeb00560184558aa451
roggenbailey <baileyroggen@gmail.com>
2025-05-29 18:24:46 -0700

    Add files via upload

---
+++ javaScript.js
@@ -0,0 +1,9 @@
+// Toggle mobile nav menu
+document.addEventListener('DOMContentLoaded', () => {
+  const toggle = document.querySelector('.menu-toggle');
+  const navLinks = document.querySelector('.nav-links');
+
+  toggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+  });
+});
